c3c392a652113ff473ad1b037a106d3c3491f192
Patryk <patryk.r.nowak@gmail.com>
2023-01-15 20:03:26 +0000

    testing function demo

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+
+/**
+ * 
+ * Return the sum of two numbers
+ * 
+ * @param integer $a The first number
+ * @param integer $b the second number
+ * 
+ * @return integer The sum of the two numbers
+ * 
+ */
+function add ($a, $b) {
+    return $a + $b;
+}
